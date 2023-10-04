fa572c219824b1f65cb3ecef7f5839b2fc06cdd2
Carl Otto Strömstedt <carl.ottostromstedt@gmail.com>
2023-10-04 14:56:12 +0200

    feat: extract user Location from Browser

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_baum():
     return "<p>Hello, Baum!</p>"
+
+@app.route("/extract")
+def extract_location():
+    return render_template("extract.html")
